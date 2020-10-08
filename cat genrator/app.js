986b68e4ab1e082f2af370364b00901d4d427547
izharabbasi <izharabbasi103@gmail.com>
2020-10-09 01:27:49 +0500

    complete Cat Generator Project

--- cat genrator/app.js
+++ cat genrator/app.js
@@ -1,6 +1,6 @@
 function genCat(){
-    var img = document.createElement('img');
+    var images = document.createElement('img');
     var div = document.getElementById('cat-gen');
-    img.src = 'https://api.thecatapi.com/api/images/get?format=src&type=gif&size=small';
-    div.appendChild(img);
+    images.src = 'https://api.thecatapi.com/api/images/get?format=src&type=gif&size=small';
+    div.appendChild(images);
 }
