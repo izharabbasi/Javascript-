6d9e321de9fe41963f383a41768d98560fefeba1
izharabbasi <izharabbasi103@gmail.com>
2020-10-12 15:58:45 +0500

    adding project 5

--- button_changer/app.js
+++ button_changer/app.js
@@ -1,51 +1,52 @@
-let allButton = document.getElementsByTagName('button');
+let allButtons = document.getElementsByTagName('button');
 
-let copyAllButton = [];
-for(let i = 0; i < allButton.length; i++){
-    copyAllButton.push(allButton[i].classList[1])
+let copyAllButtons = []
+for (let i= 0; i < allButtons.length; i++){
+    copyAllButtons.push(allButtons[i].classList[1])
 }
-console.log(copyAllButton)
-function colorChange(buttonChanger){
-    if(buttonChanger.value === 'red'){
+
+
+function colorChange(colorChanger){
+    if(colorChanger.value === 'red'){
         redButton();
     }
-    else if(buttonChanger.value === 'green'){
+    if(colorChanger.value === 'green'){
         greenButton();
     }
-    else if(buttonChanger.value === 'reset'){
+    if(colorChanger.value === 'reset'){
         resetButton();
     }
-    else if(buttonChanger.value === 'random'){
+    if(colorChanger.value === 'random'){
         randomButton();
     }
 }
 
 function redButton(){
-    for(i=0; i < allButton.length; i++){
-        allButton[i].classList.remove(allButton[i].classList[1]);
-        allButton[i].classList.add('btn-danger');
+    for(i=0; i < allButtons.length; i++){
+        allButtons[i].classList.remove(allButtons[i].classList[1]);
+        allButtons[i].classList.add('btn-danger');
     }
 }
 
 function greenButton(){
-    for(i=0; i < allButton.length; i++){
-        allButton[i].classList.remove(allButton[i].classList[1]);
-        allButton[i].classList.add('btn-success');
+    for(i=0; i < allButtons.length; i++){
+        allButtons[i].classList.remove(allButtons[i].classList[1]);
+        allButtons[i].classList.add('btn-success');
     }
 }
 
 function resetButton(){
-    for(i=0; i < allButton.length; i++){
-        allButton[i].classList.remove(allButton[i].classList[1]);
-        allButton[i].classList.add(copyAllButton[i]);
+    for(i=0; i < allButtons.length; i++){
+        allButtons[i].classList.remove(allButtons[i].classList[1]);
+        allButtons[i].classList.add(copyAllButtons[i]);
     }
 }
 
 function randomButton(){
-    var choice = ["btn-primary", "btn-danger", "btn-warning", "btn-success"]
-    for(i=0; i < allButton.length; i++){
-        let random = Math.floor(Math.random() * 4);
-        allButton[i].classList.remove(allButton[i].classList[1]);
-        allButton[i].classList.add(choice[random]);
+    let choice = ["btn-primary", "btn-danger", "btn-warning", "btn-success"]
+    for(i=0; i < allButtons.length; i++){
+        let random = Math.floor(Math.random() * 4)
+        allButtons[i].classList.remove(allButtons[i].classList[1]);
+        allButtons[i].classList.add(choice[random]);
     }
 }
--- rock,paper,scissor/app.js
+++ rock,paper,scissor/app.js
@@ -1,71 +1,75 @@
 function rpsGame(yourChoice){
     console.log(yourChoice);
-    var humanChoice , botChoice;
+    let humanChoice, botChoice;
     humanChoice = yourChoice.id;
-    botChoice = randomChoice(randomNumber());
-    console.log('Computer Choice' , botChoice)
+    console.log(humanChoice);
 
-    results = deccideWinner(humanChoice,botChoice);
+    botChoice = randomChoice(randomNumber());
+    console.log('Computer Choice', botChoice);
+
+    results = decideWinner(humanChoice, botChoice);
     console.log(results);
 
     message = finalMessage(results);
-    console.log(message)
-    rpsFrontEnd(humanChoice,botChoice,message);
+    console.log(message);
+
+    rpsFrontEnd(humanChoice, botChoice, message)
 }
 
 function randomNumber(){
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 3)
 }
 
 function randomChoice(number){
     return ['rock','paper','scissor'][number]
 }
 
-function deccideWinner(yourChoice,computerChoice){
+function decideWinner(yourChoice, compterChoice){
     let rpsDatabase = {
         'rock':{'scissor': 1, 'rock': 0.5, 'paper': 0},
         'paper':{'rock': 1, 'paper': 0.5, 'scissor': 0},
         'scissor':{'paper': 1, 'scissor': 0.5, 'rock': 0},
     };
 
-    let yourScore = rpsDatabase[yourChoice][computerChoice]
-    let botScore = rpsDatabase[computerChoice][yourChoice]
-
-    return [yourScore, botScore]
+    let yourScore = rpsDatabase[yourChoice][compterChoice];
+    let computerScore = rpsDatabase[compterChoice][yourChoice];
+    
+    return [yourScore,computerScore]
 }
 
-function finalMessage([yourScore, botScore]){
-    if (yourScore === 0){
-        return {'message' : 'You Lost!', 'color' : 'red'}
+function finalMessage([yourScore]){
+    if(yourScore === 0){
+        return {'message':'You Lost!', 'color' : 'red'}
     }
-    else if (yourScore === 0.5){
-        return {'message' : 'You tied!', 'color' : 'yellow'}
+    else if(yourScore === 0.5){
+        return {'message':'You tied!', 'color' : 'yellow'}
     }
     else{
-        return {'message' : 'You Won!', 'color' : 'green'}
+        return {'message':'You won!', 'color' : 'green'}
     }
 }
 
-function rpsFrontEnd(yourImgChoice, botImgChoice, finalMessage){
+function rpsFrontEnd(humanImgChoice, computerImgChoice, finalMessage){
     let imageDatabase = {
-        'rock': document.getElementById('rock').src,
-        'paper': document.getElementById('paper').src,
-        'scissor': document.getElementById('scissor').src,
+        'rock' : document.getElementById('rock').src,
+        'paper' : document.getElementById('paper').src,
+        'scissor' : document.getElementById('scissor').src,
     }
 
     document.getElementById('rock').remove();
     document.getElementById('paper').remove();
     document.getElementById('scissor').remove();
 
-    var humanDiv = document.createElement('div');
-    var botDiv = document.createElement('div');
-    var messageDiv = document.createElement('div');
+    let humanDiv = document.createElement('div');
+    let messageDiv = document.createElement('div');
+    let botDiv = document.createElement('div');
 
-    humanDiv.innerHTML = "<img src='" + imageDatabase[yourImgChoice] +"' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37,50,233,1)'>";
+    humanDiv.innerHTML = "<img src='" + imageDatabase[humanImgChoice] +"' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37,50,233,1)'>";
     messageDiv.innerHTML = "<h1 style='color:" + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>";
-    botDiv.innerHTML = "<img src='" + imageDatabase[botImgChoice] +"' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243,38,24,1)'>";
+    botDiv.innerHTML = "<img src='" + imageDatabase[computerImgChoice] +"' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243,38,24,1)'>";
 
     document.getElementById('flex-box-div').appendChild(humanDiv);
     document.getElementById('flex-box-div').appendChild(messageDiv);
     document.getElementById('flex-box-div').appendChild(botDiv);
 }
+
